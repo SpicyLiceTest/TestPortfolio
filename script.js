cfacb092406c96e68fe288a732d9a86aac0069c7
SpicyLiceTest <samuelyoder19@gmail.com>
2025-04-08 22:20:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.addEventListener('scroll', () => {
     const header = document.getElementById('header');
     const scrollPosition = window.scrollY;
 
-    // Fade out header when scrolling past 300px (image height)
-    header.classList.toggle('fade', scrollPosition > 100);
+    // Fade out header when scrolling past 50px
+    header.classList.toggle('fade', scrollPosition > 50);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
